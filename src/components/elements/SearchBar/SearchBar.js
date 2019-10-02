c0dc63176a6a6047035574211fd5e564aa7a646e
A F M Muzahidur Rahman <Tareq@As-MacBook-Pro.local>
2019-10-03 01:02:51 +0300

    updated

---
+++ src/components/elements/SearchBar/SearchBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
+
+import './SearchBar.css';
+
+class SeacrhBar extends Component {
+    state = {
+
+    }
+
+    render () {
+        return (
+            <div>
+                SearchBar
+            </div>
+        )
+    }
+}
+
+export default SeacrhBar;
