4468e526c55f79365e384edf55de7e138407dc5e
A F M Muzahidur Rahman <Tareq@As-MacBook-Pro.local>
2019-10-05 12:54:42 +0300

    updated

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Movie extends Component {
+    render() {
+        return (
+            <div>
+                Movie 
+            </div>
+        )
+    }
+}
+
+
+
+
+
+
+
+export default Movie;
---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>The page dosn't exist...</h1>
+        </div>
+    )
+}
+
+
+export default NotFound;
